758872f482288a67c70bda0a069427d01e81fb49
DmitriyGolenko <93517342+DmitriyGolenko@users.noreply.github.com>
2023-03-18 18:45:12 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#PBS -l walltime=00:00:50
+#PBS -l select=1:ncpus=12:ompthreads=1
+#PBS -e err.txt
+#PBS -o log.txt
+
+cd $PBS_O_WORKDIR
+echo "OMP_NUM_THREADS = $OMP_NUM_THREADS"
+echo
+./omp_one
